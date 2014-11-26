0c766446d739dd7dbd5749d0a062a8c1a2d0c94f
Claus Witt <claus@wittnezz.dk>
2014-11-26 10:04:03 +0100

    sets up bootstrapping for tests

---
+++ tests/OpenGraphParser/OpenGraphParserTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace OpenGraphParser;
+class OpenGraphParserTest extends \PHPUnit_Framework_TestCase
+{
+    public function testStuffWorks() {
+        $this->assertEquals(1, 1);
+    }
+
+
+}
+
