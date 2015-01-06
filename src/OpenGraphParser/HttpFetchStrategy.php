564171c72fbfb70a134d8548adc212361e0dd48b
Claus Witt <claus@wittnezz.dk>
2015-01-06 21:03:29 +0100

    fixes namespace for guzzle call

--- src/OpenGraphParser/HttpFetchStrategy.php
+++ src/OpenGraphParser/HttpFetchStrategy.php
@@ -2,7 +2,7 @@
 namespace OpenGraphParser;
 class HttpFetchStrategy extends AbstractFetchStrategy {
     protected function get_content($uri) {
-        $client = new GuzzleHttp\Client();
+        $client = new \GuzzleHttp\Client();
         $response = $client->get($uri);
         return $response->getBody();
     }
